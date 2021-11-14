cfe0d771c54f1644e587eb71671215bf0ce4f4d2
jifrozen <answldjs1836@naver.com>
2021-11-14 18:16:01 +0900

    [Env] 환경 세팅

---
+++ src/test/java/org/diamond_badge/footprint/FootprintApplicationTests.java
@@ -0,0 +1,13 @@
+package org.diamond_badge.footprint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootprintApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
